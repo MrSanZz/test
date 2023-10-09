9855361bcc935a32e968e3ac50d3578f34358db7
MrSanZz <95021841+MrSanZz@users.noreply.github.com>
2023-10-09 20:27:04 +0700

    Update wddos.py

--- wddos.py
+++ wddos.py
@@ -13,7 +13,7 @@ print('DDOS WITH 7 LAYER')
 print('1. UDP')
 choice = input('Select Methods : ')
 
-if choice == ('1'):
+if choice.startswith("1"):
     pass
     trgt = raw_input('IP Target : ')
     port = raw_input('Port : ')
